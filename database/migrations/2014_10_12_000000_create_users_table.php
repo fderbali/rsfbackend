2154fd67dd99cc26836047023d352cb894e67aff
Fahmi Derbali <fderbali@users.noreply.github.com>
2022-04-24 15:25:15 -0400

    Clean up et faire une partie des migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthdate');
+            $table->string('address');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->string('country');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
