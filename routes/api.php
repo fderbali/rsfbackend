327e89cb1bb44fb4249705a7044e7df9f1dbed48
Fahmi <fahmi@appwapp.com>
2022-05-12 19:53:55 -0400

    Fixer les routes et les protéger par le middleware auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -20,23 +20,33 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-
-Route::post('/training/create', [TrainingController::class, 'store']);
 Route::get('/training', [TrainingController::class, 'index']);
-Route::delete('/training/{training}', [TrainingController::class, 'delete']);
-Route::put('/training/{training}', [TrainingController::class, 'update']);
 Route::get('/training/{training}', [TrainingController::class, 'show']);
-
-Route::post('/category/create', [CategoryController::class, 'store']);
 Route::get('/category', [CategoryController::class, 'index']);
-Route::delete('/category/{category}', [CategoryController::class, 'delete']);
-Route::put('/category/{category}', [CategoryController::class, 'update']);
 Route::get('/category/{category}', [CategoryController::class, 'show']);
+// L'utilisateur quand il arrive au début il est forcément pas authentifié,
+// c'est pour ça qu'on met cette route en dehors du middleware auth:sanctum
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::post('/demand/create', [DemandController::class, 'store']);
-Route::get('/demand', [DemandController::class, 'index']);
-Route::delete('/demand/{demand}', [DemandController::class, 'delete']);
-Route::put('/demand/{demand}', [DemandController::class, 'update']);
-Route::get('/demand/{demand}', [DemandController::class, 'show']);
+Route::middleware('auth:sanctum')->group(function () {
+    // Trainings
+    Route::post('/training/create', [TrainingController::class, 'store']);
+    Route::delete('/training/{training}', [TrainingController::class, 'delete']);
+    Route::put('/training/{training}', [TrainingController::class, 'update']);
+    // Categories
+    Route::post('/category/create', [CategoryController::class, 'store']);
+    Route::delete('/category/{category}', [CategoryController::class, 'delete']);
+    Route::put('/category/{category}', [CategoryController::class, 'update']);
+
+    // Demands
+    Route::post('/demand/create', [DemandController::class, 'store']);
+    Route::get('/demand', [DemandController::class, 'index']);
+    Route::delete('/demand/{demand}', [DemandController::class, 'delete']);
+    Route::put('/demand/{demand}', [DemandController::class, 'update']);
+    Route::get('/demand/{demand}', [DemandController::class, 'show']);
+
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
 
