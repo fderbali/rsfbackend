50bccde609a99dd1a519b75d056ec85fde53fe6d
Fahmi <fahmi@appwapp.com>
2022-05-12 20:11:35 -0400

    Seeder des users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,8 +23,14 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'first_name' => $this->faker->name,
+            'last_name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'birth_date' => $this->faker->date(),
+            'address' => $this->faker->address(),
+            'city' => $this->faker->city(),
+            'country' => $this->faker->country(),
+            'zip_code' => "H2A 2T6",
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
