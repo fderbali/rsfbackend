526b7590a809c69abf29fe2fdbbd40ec732eaf60
Fahmi <fahmi@appwapp.com>
2022-05-14 21:11:33 -0400

    Register user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -24,5 +27,21 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+    }
+    public function create(UserRequest $request){
+        $user = User::create([
+            "first_name"=>$request->first_name,
+            "last_name"=>$request->last_name,
+            "email"=>$request->email,
+            "birth_date"=>$request->birth_date,
+            "address"=>$request->address,
+            "city"=>$request->city,
+            "zip_code"=>$request->zip_code,
+            "country"=>$request->country,
+            "password"=> Hash::make($request->password)
+        ]);
+        if($user) {
+            return response()->json(["success"=>true]);
+        }
     }
 }
