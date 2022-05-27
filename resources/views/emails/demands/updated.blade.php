ae455111f002744d82a2661b682d400e400a0f06
Sylvie Ratelle <ratellesylvie1@gmail.com>
2022-05-26 21:17:02 -0400

    Améliorations des emails

--- resources/views/emails/demands/updated.blade.php
+++ resources/views/emails/demands/updated.blade.php
@@ -2,11 +2,10 @@
 
 # Bonjour {{ $demand->user->first_name }} {{ $demand->user->last_name }},
 
-## Votre demande a été mise à jour
+## Votre demande de formation a été mise à jour.
 
 <p>Demande de formation : {{ $demand->training->title }}</p>
-<p>Pour: {{ $demand->training->user->first_name }} {{ $demand->training->user->last_name }}</p>
-<p>Par : {{ $demand->user->first_name }} {{ $demand->user->last_name }}</p>
+<p>Par: {{ $demand->training->user->first_name }} {{ $demand->training->user->last_name }}</p>
 <p>Status de la demande : {{ $demand->status == 'confirmed' ? 'Confirmée' : 'Annulée' }}</p>
 
 @component('mail::button', ['url' => 'http://localhost:8080'])
--- resources/views/emails/estimates/created.blade.php
+++ resources/views/emails/estimates/created.blade.php
@@ -2,9 +2,10 @@
 
 # Bonjour {{ $estimate->demand->user->first_name }} {{ $estimate->demand->user->last_name }},
 
-## Votre devis :
+## Voici votre devis pour la demande de formation qui a été acceptée.
 
 <p>Formation : {{ $estimate->demand->training->title }}</p>
+<p>Par : {{ $estimate->demand->training->user->first_name }} {{ $estimate->demand->training->user->last_name }}</p>
 <p>Prix : {{ $estimate->demand->training->price }} $CAD</p>
 <p>Taxes : {{ sprintf("%.2f", $estimate->demand->training->price * 0.15) }} $CAD</p>
 <p>Prix TTC : {{ $estimate->price }} $CAD</p>
